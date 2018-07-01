d84e968ecceee967fc718247dc09da4c483d830c
f0s3 <power.aerozoll@gmail.com>
2018-07-01 10:38:09 +0300

    #4 Login seems to work fine.

--- src/app.js
+++ src/app.js
@@ -3,15 +3,70 @@ const cors = require('cors')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const mysql = require('mysql')
+const crypto = require('crypto');
+const config = require('../config/config');
 const app = express()
-const port = 4080
 
 app.use(cors())
 app.use(bodyParser.json())
 app.use(morgan('combined'))
 
-app.get('/', (req, res) => {
-	res.send({message: "Hello world!"})
+
+// connection.connect((err) => {
+// 	if (err) throw err
+//
+// 	connection.query(`USE ${req.query.faculty};`, (err) => {
+// 		if (err) throw err
+// 	})
+//
+// 	connection.query(`SELECT ${req.query.weekdays} FROM ${req.query.group};`, (err, result) => {
+// 		if (err) throw err
+// 		res.json(result)
+// 	})
+// })
+
+app.post('/addGroup', (req, res) => {
 })
 
-app.listen(port, () => console.log('App is listening on port ' + port + '!'))
+app.post('/editGroup', (req, res) => {
+})
+
+app.delete('/deleteGroup', (req, res) => {
+})
+
+app.get('/showGroup', (req, res) => {
+	res.send({
+		message: `Hi there! group is: ${req.query.group}.`
+	});
+})
+
+app.post('/login', (req, res) => {
+	//encrypt password in sha256;
+	let passwordEncrypted = crypto.createHash("sha256").update(req.body.password).digest("hex");
+
+	//TODO: get encrypted password from db;
+	let loginConnection = mysql.createConnection({
+		host:config.host_DB,
+		user:config.user_DB_Login,
+		password:config.password_DB_Login
+	});
+	loginConnection.connect((err) => {
+		if (err) throw err
+		loginConnection.query(`USE SyllabusPanel_U;`, (err) => {
+			if (err) throw err
+		})
+		loginConnection.query(`SELECT password FROM users WHERE username='${req.body.username}';`, (err, result) => {
+			if (err) throw err
+			//res.json(result)
+			let passwordFromDB = result[0].password;
+			switch (req.body.username) {
+				case 'NNIEP':case 'NNIIM':case 'NNIIOT':case 'NNIIF':
+				case 'NNIPOSRM':case 'NNIPN':case 'NNIUFSK':case 'NNIFKSZ':
+				case 'FPSY':case 'FOTIUS':case 'NNCIMO':case 'IDPPO':
+				(req.body.password === passwordFromDB) ? res.status(202).send() : res.status(401).send();break;
+			}
+		})
+	})
+})
+
+app.listen(config.port || 4080, () => console.log('App is listening on port ' + config.port + '!'))
